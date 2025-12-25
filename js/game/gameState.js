884fb5483956ea827ee0d5542ceb5f4485ce1fbc
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-12-25 12:38:52 -0700

    Update gameState.js

--- js/game/gameState.js
+++ js/game/gameState.js
@@ -1,30 +1,29 @@
+/// gameState.js
+/// Central mutable game state
+/// Made by CCVO - CanC-Code
+
 export const state = {
   room: 1,
   bites: 0,
-  speed: 6,
-  gridSize: 1,
-  roomSize: 12,
-  bitesPerRoom: 15,
+  speed: 2,
   alive: true,
-  doorOpen: false
+  doorOpen: false,
+  roomSize: 12,
+  bitesPerRoom: 15
 };
 
 export function resetGameState() {
   state.room = 1;
   state.bites = 0;
-  state.speed = 6;
+  state.speed = 2;
   state.alive = true;
   state.doorOpen = false;
 }
 
 export function registerBite() {
   state.bites++;
-  state.speed += 0.3;
-  if (state.bites % state.bitesPerRoom === 0) state.doorOpen = true;
-}
-
-export function advanceRoom() {
-  state.room++;
-  state.speed += 1;
-  state.doorOpen = false;
+  state.speed += 0.05;
+  if (state.bites % state.bitesPerRoom === 0) {
+    state.doorOpen = true;
+  }
 }
